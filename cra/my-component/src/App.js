efeb4724c7db0e8b13e61ee375da586d31aff91f
ongi <>
2025-08-07 11:34:07 +0900

    0807

---
+++ cra/my-component/src/App.js
@@ -0,0 +1,105 @@
+// cra, vite 방식
+import { useState } from "react";
+// cdn 방식
+// const { useState } = React;
+
+function App() {
+  // const [변수, 함수] = setUsestate(초깃값);
+    const [mode, setMode] = useState("WELCOME");
+    const [id, setId] = useState(null);
+
+    const topics = [
+        { id: 1, title: "html", body: "html is ..." },
+        { id: 2, title: "css", body: "css is ..." },
+        { id: 3, title: "javascript", body: "javascript is ..." },
+    ];
+
+    let content = null;
+    if (mode === "WELCOME") {
+        content = <Article title="welcome" body="Hello, WEB"></Article>;
+    } else if (mode === "READ") {
+        let title,
+            body = null;
+        for (let i = 0; i < topics.length; i++) {
+            if (topics[i].id === id) {
+                title = topics[i].title;
+                body = topics[i].body;
+            }
+        }
+        content = <Article title={title} body={body}></Article>;
+    }
+
+    return (
+        <div>
+            <Header
+                subject="Web"
+                onChangeMode={() => {
+                    setMode("WELCOME");
+                }}
+            ></Header>
+            <Nav
+                topics={topics}
+                onChangeMode={(_id) => {
+                    setMode("READ");
+                    setId(_id);
+                }}
+            />
+            {content}
+        </div>
+    );
+}
+
+function Header(props) {
+    return (
+        <header>
+            <h1>
+                <a
+                    href="/"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        props.onChangeMode();
+                    }}
+                >
+                    {props.subject}
+                </a>
+            </h1>
+        </header>
+    );
+}
+
+function Nav(props) {
+    const lis = [];
+    for (let i = 0; i < props.topics.length; i++) {
+        let t = props.topics[i];
+        lis.push(
+            <li key={t.id}>
+                <a
+                    id={t.id}
+                    href={"/read/" + t.id}
+                    onClick={(e) => {
+                        e.preventDefault();
+                        props.onChangeMode(Number(e.target.id));
+                    }}
+                >
+                    {t.title}
+                </a>
+            </li>
+        );
+    }
+    return (
+        <nav>
+            <ol>{lis}</ol>
+        </nav>
+    );
+}
+
+function Article(props) {
+    return (
+        <article>
+            <h2>{props.title}</h2>
+            {props.body}
+        </article>
+    );
+}
+
+export default App;
